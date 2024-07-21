29ee1453d01c8d3ed32d6106d3311ac3108a4208
Gabriel-Villa <VillaCapurroGabriel@gmail.com>
2024-07-21 14:30:30 -0500

    upload Header and footer

---
+++ wp-content/themes/aquila/footer.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Footer File.
+ * 
+ * @package Aquila
+ */
+
+?>
+
+<footer>
+        This is my footer
+    </footer>
+
+</body>
+
+</html>
--- wp-content/themes/aquila/index.php
+++ wp-content/themes/aquila/index.php
@@ -1,22 +1,18 @@
 <?php
-    /**
-     * Main template file
-     * 
-     * @package Aquila
-     */
+
+/**
+ * Main template file
+ * 
+ * @package Aquila
+ */
+
+get_header();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
+<div class="content">
+    Content
+</div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Theme</title>
-</head>
-
-<body>
-    <h1>Hello world</h1>
-</body>
-
-</html>
+<?php
+get_footer();
+?>
