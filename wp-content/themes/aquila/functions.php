74869227f01430ab54cf1f77e4e7129acab64d32
Gabriel-Villa <VillaCapurroGabriel@gmail.com>
2024-07-23 16:15:32 -0500

    Enqueue styles and scripts

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -20,6 +20,14 @@ function aquila_enqueue_scripts()
         filemtime(get_template_directory() . '/style.css'),
         'all'
     );
+
+    wp_enqueue_script(
+        'main.js',
+        get_template_directory_uri() . "/assets/main.js",
+        [],
+        filemtime(get_template_directory() . '/assets/main.js'),
+        true
+    );
 }
 
 add_action('wp_enqueue_scripts', 'aquila_enqueue_scripts');
